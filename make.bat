4e1b529ef76163ce8fb3130cb0bf1cc15f53f0a6
carlosmsx@toshiba <carlosmsx@gmail.com>
2019-11-26 08:29:04 -0300

    new extensions

--- make.bat
+++ make.bat
@@ -1,17 +1,1 @@
-If not exist .\release md .\release
-
-sdcc -mz80 --no-std-crt0 --no-peep --code-loc 0x4000 --data-loc 0x6000 msxiot.c 
-
-hex2bin -e bin msxiot.ihx 
-copy msxiot.bin msxiot.rom
-fillfile msxiot.rom 16384 
-
-@move *.ihx ./release > nul
-@move *.noi ./release > nul
-@move *.sym ./release > nul
-@move *.lst ./release > nul
-@move *.map ./release > nul
-@move *.lk  ./release > nul
-@move *.rel ./release > nul
-@move *.asm ./release > nul
-@move *.bin ./release > nul
+..\sjasm\sjasmplus -c msxiot.asm
--- msxiot.asm
+++ msxiot.asm
@@ -1,65 +1,64 @@
-; Example of String handling in BASIC CALL-commands
-; Made By: NYYRIKKI 16.11.2011
+;Author: Carlos Escobar
+;ROM para el cartucho MSX-IoT
 
-
-        OUTPUT "PRINT.ROM"
+        OUTPUT "MSXIOT.ROM"
         ORG     #4000
 
 ;---------------------------
-
 ; External variables & routines
 CHPUT   EQU     #A2
-CALBAS	EQU	#159
+CALBAS	EQU		#159
 ERRHAND EQU     #406F
 FRMEVL  EQU     #4C64
 CHRGTR  EQU     #4666
 VALTYP  EQU     #F663
 USR     EQU     #F7F8
-PROCNM	EQU	#FD89
+PROCNM	EQU		#FD89
+CMDPORT	EQU		1
+DATPORT	EQU		0
+
+;---------------------------
+; ROM Header
+
+        DEFW    #4241,0,HANDLER,0,0,0,0,0
 
 ;---------------------------
 
-; ROM-file header
+; BASIC CALL EXTENSION handler
 
-        DEFW    #4241,0,CALLHAND,0,0,0,0,0
-
-
-;---------------------------
-
-; General BASIC CALL-command handler
-
-CALLHAND:
+HANDLER:
 
 	PUSH    HL
-	LD	HL,CMDS	        ; Table with "_" commands
+	LD		HL,CMDS	        ; Table with CALL extensions
 .CHKCMD:
-	LD	DE,PROCNM
-.LOOP:	LD	A,(DE)
-	CP	(HL)
-	JR	NZ,.TONEXTCMD	; Not equal
-	INC	DE
-	INC	HL
-	AND	A
-	JR	NZ,.LOOP	; No end of command name, go checking
-	LD	E,(HL)
-	INC	HL
-	LD	D,(HL)
-	POP	HL		; routine address
+	LD		DE,PROCNM
+.LOOP:		
+	LD		A,(DE)
+	CP		(HL)
+	JR		NZ,.TONEXTCMD	; Not equal
+	INC		DE
+	INC		HL
+	AND		A
+	JR		NZ,.LOOP	; No end of command name, go checking
+	LD		E,(HL)
+	INC		HL
+	LD		D,(HL)
+	POP		HL		; routine address
 	CALL	GETPREVCHAR
 	CALL	.CALLDE		; Call routine
-	AND	A
+	AND		A
 	RET
 
 .TONEXTCMD:
 	LD	C,0FFH
 	XOR	A
-	CPIR			; Skip to end of commandname
+	CPIR			; Skip to end of command name
 	INC	HL
-	INC	HL		; Skip address
+	INC	HL			; Skip address
 	CP	(HL)
 	JR	NZ,.CHKCMD	; Not end of table, go checking
 	POP	HL
-        SCF
+    SCF
 	RET
 	
 .CALLDE:
@@ -71,60 +70,82 @@ CMDS:
 
 ; List of available commands (as ASCIIZ) and execute address (as word)
 
-	DEFB	"UPRINT",0      ; Print upper case string
-	DEFW	_UPRINT
+	DEFB	"WLIST",0      ; 
+	DEFW	_WLIST
 	
-	DEFB	"LPRINT",0      ; Print lower case string
-	DEFW	_LPRINT
+	DEFB	"WNET",0     ; 
+	DEFW	_WNET
 
 	DEFB	0               ; No more commands
 
 ;---------------------------
-_UPRINT:
-	CALL	EVALTXTPARAM	; Evaluate text parameter
+_WLIST:
 	PUSH	HL
-        CALL    GETSTRPNT
-.LOOP
-        LD      A,(HL)
-        CALL    .UCASE
-        CALL    CHPUT  ;Print
-        INC     HL
-        DJNZ    .LOOP
-
-	POP	HL
+	LD		A,#10
+	OUT		(CMDPORT),A
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+.LOOP1
+	IN		A,(CMDPORT)
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	AND		#80
+	JR		NZ,.LOOP1
+.LOOP2
+	IN		A,(CMDPORT)
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	AND		#40
+	JR		Z,.END
+	IN		A,(DATPORT)
+	CALL	CHPUT
+	JR		.LOOP2
+.END	
+	POP		HL
 	OR      A
 	RET
-
-.UCASE:
-        CP      "a"
-        RET     C
-        CP      "z"+1
-        RET     NC
-        AND     %11011111
-        RET
+	
 ;---------------------------
-_LPRINT:
+_WNET:
 	CALL	EVALTXTPARAM	; Evaluate text parameter
 	PUSH	HL
-        CALL    GETSTRPNT
+    CALL    GETSTRPNT
 .LOOP
-        LD      A,(HL)
-        CALL    .LCASE
-        CALL    CHPUT  ;Print
-        INC     HL
-        DJNZ    .LOOP
+    LD      A,(HL)
+    CALL    .LCASE
+    CALL    CHPUT  ;Print
+    INC     HL
+    DJNZ    .LOOP
 
 	POP	HL
 	OR      A
 	RET
 
 .LCASE:
-        CP      "A"
-        RET     C
-        CP      "Z"+1
-        RET     NC
-        OR      %00100000
-        RET
+    CP      "A"
+    RET     C
+    CP      "Z"+1
+    RET     NC
+    OR      %00100000
+    RET
 ;---------------------------
 
 GETSTRPNT:
@@ -132,26 +153,26 @@ GETSTRPNT:
 ; HL = String Address
 ; B  = Lenght
 
-        LD      HL,(#F7F8)
-        LD      B,(HL)
-        INC     HL
-        LD      E,(HL)
-        INC     HL
-        LD      D,(HL)
-        EX      DE,HL
-        RET
+	LD      HL,(#F7F8)
+	LD      B,(HL)
+	INC     HL
+	LD      E,(HL)
+	INC     HL
+	LD      D,(HL)
+	EX      DE,HL
+	RET
 
 EVALTXTPARAM:
 	CALL	CHKCHAR
 	DEFB	"("             ; Check for (
 	LD	IX,FRMEVL
 	CALL	CALBAS		; Evaluate expression
-        LD      A,(VALTYP)
-        CP      3               ; Text type?
-        JP      NZ,TYPE_MISMATCH
+	LD      A,(VALTYP)
+	CP      3               ; Text type?
+	JP      NZ,TYPE_MISMATCH
 	CALL	CHKCHAR
 	DEFB	")"             ; Check for )
-        RET
+	RET
 
 
 CHKCHAR:
@@ -170,14 +191,14 @@ GETPREVCHAR:
 
 
 TYPE_MISMATCH:
-        LD      E,13
-        DB      1
+	LD      E,13
+	DB      1
 
 SYNTAX_ERROR:
-        LD      E,2
+	LD      E,2
 	LD	IX,ERRHAND	; Call the Basic error handler
 	JP	CALBAS
 	
 ;---------------------------
 
-        DS      #8000-$
+	DS      #8000-$
